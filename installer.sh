fabd2687c73a912c91121aa063687fc57c26ec47
San's <fsanches2002@outlook.com>
2025-01-20 15:36:42 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 echo "type your user"
 read nome
-sed -i 's/YourUser/$nome/' apple.service
+sed -i "s/YourUser/$nome/" apple.service
 sudo cp -r apple.service -t /etc/systemd/system/
 sudo cp -r apple.sh -t ~/Documents/
 sudo systemctl daemon-reload
